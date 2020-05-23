0a971c2c2e12aeb189da346ca3a2138dd41ea39b
chaiwanlin <50112512+chaiwanlin@users.noreply.github.com>
2020-05-23 23:12:26 +0800

    Delete eg.sh

--- eg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-## Example: a typical script with several problems
-output=$(ls ./*.m3u)
-for f in $output
-do
-  grep -qi "hq.*mp3 $f \ ' && echo "Playlist $f contains a HQ file in mp3 format'
-done
